2945b88c049b73e3571787026ecfd2ef02b9b961
spykerNZ <jrspyker@gmail.com>
2023-09-16 11:54:45 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# One Pace To Plex
+
+This guide explains how to setup One Pace for Plex
+
+## Requirements
+
+- Plex
+- Python 3.7+
+- XBMCnfoTVImporter
+
+## Install Instructions
+
+### 1. Copy Folder Structre
+
+Download this repo as a zip file then copy the "One Pace" folder to your Anime or TV folder as used in Plex.
+
+```
+    └───media
+        ├───anime
+        │   ├───One Pace
+        ├───movies
+        └───tvshowsW
+```
+
+### 2. Download One Pace Episodes
+
+Download all the one pace episodes you wish to add and place them in their respective season folders.
+
+ - Keep the default One Pace episode naming (do not make any changes!)
+ - This project currently supports up to the Drum Island arc. (Feel free to contribute to help adding more!)
+ - You can delete any season folders you don't want to include, and add them later if you wish.
+
+```
+    └───media
+        ├───anime
+        │   ├───One Pace    
+        │   │   ├───Season 01
+        │   │   │   ├───[One Pace][1] Romance Dawn 01 [1080p][FB72C13F].mkv     
+        │   │   │   └───One Pace - S01E01 - Romance Dawn, the Dawn of an Adventure.nfo           
+        │   │   └───Season 02
+        │   │       ├───[One Pace][8-11] Orange Town 01 [1080p][2388DB63].mkv    
+        │   │       └───One Pace - S02E01 - Enter Nami.nfo      
+        ├───movies
+        └───tvshows
+```
+
+
+### 3. File Renaming
+
+1. Copy the rename.py, seasons.json and exceptions.json file to your One Pace directory
+
+```
+    └───media
+        └───anime
+            └───One Pace
+                ├───Season 01
+                └───Season 02
+                └───rename.py
+                └───seasons.json
+                └───exceptions.json
+```
+
+2.  Open a shell/powershell terminal.
+
+2.  Change directory to where your One Pace script folder is: `cd /media/anime/One Pace/`
+
+4.  Run the script in dry-run mode to see what change would occur (you can try with Docker or Python):
+    a) Python: `python3 rename.py --dry-run` or `python rename.py --dry-run`
+    b) Docker: `docker run --rm -v "$PWD":/data -w="/data" python:3 python rename.py --dry-run`
+
+5.  Once you are okay with the changes you see, remove the `--dry-run` flag from the command and run it again.
+    Your files will be renamed to the corresponding One Piece episode, i.e.:
+    "[One Pace][1] Romance Dawn 01 [1080p][FB72C13F].mkv" -> "One Pace - S01E01 - Romance Dawn, the Dawn of an Adventure.mkv"
+
+### 4. Install XBMCnfoTVImporter
+
+You need to install [XBMCnfoTVImporter](https://github.com/gboudreau/XBMCnfoTVImporter.bundle) for plex in order to scan in One Pace. Follow the instructions and install.
+
+### 5. Scan In Plex
+
+You need to swap to the XBMCnfoTVImporter agent in Plex to scan your new One Pace folder. 
+
+1. Open Plex
+2. Navigate to your anime library
+3. Click 'Manage Library' -> 'Edit...'
+4. Click on the 'Advanced' tab
+5. Click the 'Agent' dropdown box. (and note what you have set to currently so you can change it back)
+6. Select the 'XBMCnfoTVImporter' option. (If this does not exist XBMCnfoTVImporter may not be installed correctly)
+7. Click "Save Changes"
+8. Click "Scan Library Files"
+9. One Pace should get scanned into your library
+10. Once it is complete, change your agent back to what you had originally!
+    
+Notes: 
+- This method will scan in One Pace without interfering with your existing shows.
+- Don't press "Refresh all metadata" in plex as this may mess up your other shows.
+
+## Acknowledgements
+
+- Thanks to [@tomatoshadow](https://github.com/Tomatoshadow) for creating the original nfos and instructions for this plex setup
+- Inspired by this alternative plex setup [one-pace-to-plex](https://github.com/Matroxt/one-pace-to-plex) by [@Matroxt](https://github.com/Matroxt)
+- Shoutout to the [One Pace Team](https://onepace.net) - thanks for putting together this amazing project!
---
+++ rename.py
@@ -0,0 +1,169 @@
+import argparse
+import dataclasses
+import re
+import json
+import os
+import sys
+from pathlib import Path
+from typing import Optional
+
+args = None
+
+SEASONS_JSON = "seasons.json"
+EXCEPTIONS_JSON = "exceptions.json"
+SHOW_NAME = "One Pace"
+MKV_EXT = ".mkv"
+NFO_EXT = ".nfo"
+
+
+@dataclasses.dataclass
+class Episode:
+    show: str
+    season: int
+    number: int
+    title: Optional[str] = None
+
+    @property
+    def episode_id(self) -> str:
+        return f"S{self.season:02d}E{self.number:02d}"
+
+    def get_file_name(self, extension: str = MKV_EXT):
+        return f"{self.show} - {self.episode_id} - {self.title}{extension}"
+
+
+def get_episode_from_id(show_name: str, id: str) -> Optional[Episode]:
+    id_pattern = r"S(\d+)E(\d+)"
+    match = re.search(id_pattern, id)
+    if match:
+        return Episode(
+            show=show_name, season=int(match.group(1)), number=int(match.group(2))
+        )
+
+
+def get_episode_from_nfo(filename: str) -> Optional[Episode]:
+    nfo_pattern = r"^(.*?) - S(\d+)E(\d+) - (.*?)\.nfo$"
+    match = re.search(nfo_pattern, filename)
+    if match:
+        return Episode(
+            show=match.group(1),
+            season=int(match.group(2)),
+            number=int(match.group(3)),
+            title=match.group(4),
+        )
+
+
+def get_episode_from_mkv(filename: str, seasons: dict[str, int]) -> Optional[Episode]:
+    mkv_pattern = (
+        r"\[One Pace\]\[(.*?)\]\s(.*?)\s(\d{1,2}(?:-\d{1,2})?)\s\[(.*?)\]\[(.*?)\]\.mkv"
+    )
+    match = re.search(mkv_pattern, filename)
+    if match:
+        season_title = match.group(2)
+        episode_number = int(match.group(3))
+        season_number = seasons.get(season_title)
+        return Episode(
+            show=SHOW_NAME,
+            season=season_number,
+            number=episode_number,
+        )
+
+
+def debugger_is_active() -> bool:
+    return hasattr(sys, "gettrace") and sys.gettrace() is not None
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Rename One Pace files to matching .nfo file format"
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        nargs="?",
+        help="Data directory (path to where the root One Pace folder is)",
+        default=None,
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="If this flag is passed, the output will only show how the files would be renamed",
+    )
+    args = vars(parser.parse_args())
+
+    if (arg_dir := args.get("directory")) is None:
+        show_dir = Path.cwd()
+    else:
+        show_dir = Path(arg_dir)
+
+    dry_run = args.get("dry_run")
+
+    if debugger_is_active():
+        dry_run = True
+        show_dir = Path.cwd() / (SHOW_NAME + " - Debug")
+
+    with open(SEASONS_JSON, "r") as json_file:
+        seasons: dict[str, int] = json.load(json_file)
+
+    with open(EXCEPTIONS_JSON, "r") as json_file:
+        exceptions: dict[dict[str, int]] = json.load(json_file)
+
+    # create a lookup table of nfo data
+    nfo_data_lookup: dict[tuple(int, int), Episode] = {}
+    nfo_files = show_dir.rglob(f"*{NFO_EXT}")
+    for filepath in nfo_files:
+        nfo_data = get_episode_from_nfo(filepath.name)
+        if nfo_data is not None:
+            nfo_data_lookup[(nfo_data.season, nfo_data.number)] = nfo_data
+
+    # create a pending rename file list
+    pending: list[tuple[str, Episode]] = []
+
+    # iterate over all mkv files
+    mkv_files = show_dir.rglob(f"*{MKV_EXT}")
+    for filepath in mkv_files:
+        episode = get_episode_from_mkv(filepath.name, seasons)
+        if episode is None:
+            continue
+        pending.append((str(filepath), episode))
+
+    # iterate over all exceptions
+    for season in exceptions:
+        season_folder = Path(show_dir / season)
+        mkv_files = season_folder.rglob(f"*{MKV_EXT}")
+        season_files = [filepath for filepath in mkv_files]
+        exception_mapping: dict[str, int] = exceptions.get(season)
+        for exception_str in exception_mapping:
+            matches = []
+            for filepath in season_files:
+                if exception_str in filepath.name:
+                    matches.append(filepath)
+            if len(matches) >= 2:
+                print("Warning! Multiple exception episodes found:")
+                for match in matches:
+                    print(match)
+                continue
+            elif len(matches) == 1:
+                episode_number = exception_mapping.get(exception_str)
+                season_number = int(season.split(" ")[-1])
+                pending.append(
+                    (str(matches[0]), Episode(SHOW_NAME, season_number, episode_number))
+                )
+
+    # rename all files
+    for filepath, episode in pending:
+        nfo_data = nfo_data_lookup.get((episode.season, episode.number))
+        if nfo_data is None:
+            continue
+        episode.title = nfo_data.title
+        new_episode_name = episode.get_file_name()
+
+        if dry_run:
+            print('DRYRUN: "{}" -> "{}"'.format(Path(filepath).name, new_episode_name))
+            continue
+
+        print('RENAMING: "{}" -> "{}"'.format(Path(filepath).name, new_episode_name))
+        os.rename(filepath, Path(filepath).parent.absolute() / new_episode_name)
+
+
+if __name__ == "__main__":
+    main()
